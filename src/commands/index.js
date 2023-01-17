db52a5a943482e51a2e87e47975f9a088716dceb
Mohamad Haidil Idris <mohamadhaidilidris@hotmail.com>
2023-01-17 17:43:47 +0800

    add chatting command

--- src/commands/index.js
+++ src/commands/index.js
@@ -4,8 +4,8 @@ import urban from "./urban.js";
 import setAfk from "./afk/setAfk.js";
 import CommandList from "./commandList.js";
 import Help from "./help.js";
-import Time from "./time.js";
-let cooldown = {}
+import Time from "./time.js"
+import openai from "./openai.js";
 
 const commands = (channel, tags, message, client) => {
   const command = message[0].toLowerCase()
@@ -16,6 +16,7 @@ const commands = (channel, tags, message, client) => {
   if (command === 'commands') CommandList(client, channel)
   if (command === 'help') Help(client, channel, message)
   if (command === 'time') Time(client, channel, message)
+  if (command === 'chatting') openai(client, channel, message, tags, 600000)
 };
 
 export default commands;
--- src/commands/pyramid.js
+++ src/commands/pyramid.js
@@ -6,7 +6,7 @@ const pyramid = async (client, channel, message, cooldownDuration) => {
   const fileLocation = 'src/models/cooldown.json'
   let cooldown = await ReadFile(fileLocation);
 
-  if (!cooldown.pyramid) {
+  if (!cooldown?.[channel]?.['pyramid']) {
     if (message.length === 2) {
       const limit = 3;
       const args = message[1].concat(" ")
@@ -15,7 +15,10 @@ const pyramid = async (client, channel, message, cooldownDuration) => {
         for (let i = 1; i <= limit; i++) client.say(channel, args.repeat(i))
         for (let i = (limit - 1); i > 0; i--) client.say(channel, args.repeat(i))
 
-        SetCooldown(cooldown, cooldownDuration, fileLocation);
+        let cooldownValue = true
+        let afterCdValue = false
+
+        SetCooldown(cooldown, cooldownDuration, fileLocation, cooldownValue, afterCdValue, channel, 'pyramid');
       } else {
         client.say(channel, 'Sadeg exceed emote limit');
       }
